2f7e263d8aa6d2fe4af1ec067ecbef1748abb30e
Mayeraa <mayeraas@gmail.com>
2024-12-23 21:55:18 +0530

    connected to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,24 @@
-import express from 'express';
+import express from 'express';  //set up express server
+import mongoose from 'mongoose'; //use mongodb cloud service
+import dotenv from 'dotenv'; //use hidden file 
 
+dotenv.config();
+
+//this connects our project to cluster
+mongoose
+    .connect(process.env.MONGO)
+    .then(()=> {
+        console.log("MongoDB is connected");
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+
+//this hosts the server
 const app = express();
 
+//this checks for server on a particular port
 app.listen(3000, ()=>{
     console.log("Server is running on port 3000");
 });
+
