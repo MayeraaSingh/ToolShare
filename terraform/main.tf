aa0de2b83225b92932b91c0ec4eca5c7fcd6037d
MayeraaSingh <mayeraas@gmail.com>
2026-03-28 16:49:00 +0530

    new instance yet again

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+# Security Group
 resource "aws_security_group" "devsec_sg" {
   name        = "devsec-security-group"
   description = "Security group for DevSec pipeline"
@@ -21,15 +22,22 @@ resource "aws_security_group" "devsec_sg" {
   }
 
   ingress {
-    from_port   = 3000
-    to_port     = 3000
+    from_port   = 30007
+    to_port     = 30007
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 5173
-    to_port     = 5173
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -42,15 +50,27 @@ resource "aws_security_group" "devsec_sg" {
   }
 }
 
+# EC2 Instance
 resource "aws_instance" "devsec_server" {
-  ami           = "ami-0f5ee92e2d63afc18"
-  instance_type = "t3.small"
+  ami           = "ami-0f5ee92e2d63afc18" # Ubuntu
+  instance_type = "m7i-flex.large"
 
   key_name = "devsec-key"
 
   vpc_security_group_ids = [aws_security_group.devsec_sg.id]
 
+  # Storage (40GB)
+  root_block_device {
+    volume_size = 40
+    volume_type = "gp3"
+  }
+
   tags = {
     Name = "devsec-pipeline-server"
   }
+}
+
+# Elastic IP
+resource "aws_eip" "devsec_eip" {
+  instance = aws_instance.devsec_server.id
 }
